502cadce241cb1ddb3751f6a07e19310cb276f31
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2022-02-08 16:10:00 -0800

    testing for the capybara

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,4 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require "capybara/rails"
-require "capybara/rspec"
-require "capybara/poltergeist" # Add this line to require poltergeist
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 
@@ -10,6 +7,9 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require "capybara/rails"
+require "capybara/rspec"
+require "capybara/poltergeist" # Add this line to require poltergeist
 # Add additional requires below this line. Rails is not loaded until this point!
 
 Capybara.javascript_driver = :poltergeist
